1b3b4ed893a57cdd1d8583ec1a68f3f3ab9b548f
Cristian Guerrero <cristian@s9.cl>
2016-07-04 01:46:14 -0700

    add rspec for user model, add factory for valid user, add validations for user, add full_name method for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true, format: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :password, length: { minimum: 8 }
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,75 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context "validations" do
+
+    it "is valid with a firstname, lastname, email and password" do
+      u = User.new FactoryGirl.attributes_for(:user)
+      expect(u).to be_valid
+    end
+
+    it "is invalid without a first name" do
+      u = User.new FactoryGirl.attributes_for(:user).merge({first_name: nil})
+      expect(u).to be_invalid
+    end
+
+    it "is invalid without a last name" do
+      u = User.new FactoryGirl.attributes_for(:user).merge({last_name: nil})
+      expect(u).to be_invalid
+    end
+
+    it "is invalid without an email address" do
+      u = User.new FactoryGirl.attributes_for(:user).merge({email: nil})
+      expect(u).to be_invalid
+    end
+
+    it "is an invalid email if it doesn't have a (dot) domain" do
+      u = User.new FactoryGirl.attributes_for(:user).merge({email: "test@test"})
+      expect(u).to be_invalid
+    end
+
+    it "is an invalid email if it doesn't have an @" do
+      u = User.new FactoryGirl.attributes_for(:user).merge({email: "test.com"})
+      expect(u).to be_invalid
+    end
+
+    it "is invalid with a duplicate email address" do
+      u = FactoryGirl.create(:user)
+      u2 = User.new FactoryGirl.attributes_for(:user).merge({email: u.email})
+      expect(u2).to be_invalid
+    end
+
+    it "is invalid without a password" do
+      u = User.new FactoryGirl.attributes_for(:user).merge({password: nil, password_confirmation: nil})
+      expect(u).to be_invalid
+    end
+
+    it "is invalid with a short password" do
+      u = User.new FactoryGirl.attributes_for(:user).merge({password: 'short', password_confirmation: 'short'})
+      expect(u).to be_invalid
+    end
+
+    it "is invalid with a password confirmation mismatch" do
+      u = User.new FactoryGirl.attributes_for(:user).merge({password: 'P4SSw0rd', password_confirmation: 'dr0wSS4P'})
+      expect(u).to be_invalid
+    end
+
+  end
+
+  context "full_name method" do
+    it "returns a concatenated first and last name" do
+      u = User.new FactoryGirl.attributes_for(:user).merge({first_name: "John", last_name: "Doe"})
+      expect(u.full_name).to eq("John Doe")
+    end
+  end
+
+  context "hashing the password method" do
+    it "generates a password digest" do
+      u = User.new FactoryGirl.attributes_for(:user)
+      u.save
+      expect(u.password_digest).to be
+    end
+  end
+
 end
